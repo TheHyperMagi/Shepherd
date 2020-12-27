03a9548a0e901e6188a652cbe610acf3261de215
Jonathan Seng <zee.hatcher010@gmail.com>
2020-12-28 01:05:50 +0800

    replace Shepherd node in Field with Shepherd scene

--- Shepherd/Scripts/Shepherd.gd
+++ Shepherd/Scripts/Shepherd.gd
@@ -12,18 +12,20 @@ func control_loop(delta):
 	VELOCITY.z = 0
 	var input = Vector3()
 	if Input.is_action_pressed("move_forwards"):
-		input.z += 1
-	if Input.is_action_pressed("move_backwards"):
 		input.z -= 1
+	if Input.is_action_pressed("move_backwards"):
+		input.z += 1
 	if Input.is_action_pressed("move_left"):
-		input.x += 1
-	if Input.is_action_pressed("move_right"):
 		input.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input.x += 1
 	
 	input = input.normalized()
 	
 	if input != Vector3(0,0,0):
-		rotate_from_movement(input)
+		pass
+#		rotate_from_movement(input)
+
 	
 	#var direction = (transform.basis.x * input.x + transform.basis.z * input.z)
 	#velocity.x = direction.x * speed
