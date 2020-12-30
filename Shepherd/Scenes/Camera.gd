2784905caffc9e1f1af5e901320e849bf1d8f9b9
Zircon <zircon332@gmail.com>
2020-12-30 19:16:16 +0800

    Fixed camera zoom, added unversioned changes I forgot from previous push

---
+++ Shepherd/Scenes/Camera.gd
@@ -0,0 +1,30 @@
+extends Camera
+
+export onready var target = get_node("../Shepherd")
+
+export (Vector3) var offset := Vector3(20, 10, 20)
+export (float) var zoom_in_limit := 10.0
+export (float) var zoom_out_limit := 40.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	translation = target.translation + offset
+	look_at_target()
+
+func _process(delta):
+	zoom_input()
+	translation = target.translation + offset
+
+func look_at_target():
+	look_at(target.translation, Vector3.UP)
+
+# Zoom in and out by moving the camera's offset with lerp
+func zoom_input():
+	if Input.is_action_just_released("zoom_in"):
+		if translation.distance_to(target.translation) > zoom_in_limit:
+			offset = lerp(offset, Vector3.ZERO, 0.1)
+			look_at_target()
+	if Input.is_action_just_released("zoom_out"):
+		if translation.distance_to(target.translation) < zoom_out_limit:
+			offset = lerp(offset, Vector3.ZERO, -0.1)
+			look_at_target()
