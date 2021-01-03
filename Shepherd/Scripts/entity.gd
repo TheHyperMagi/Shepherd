982517675ca46ddf78e3c7bed3071d320dfb7ca7
thehypermagi <shamil.haqeem@gmail.com>
2021-01-04 07:37:44 +0800

    Revert commit: f2f1f73acc5c4fdc892a4389ebd2140ac6128175

--- Shepherd/Scripts/entity.gd
+++ Shepherd/Scripts/entity.gd
@@ -1,11 +1,8 @@
 extends KinematicBody
 
-export var ACCELERATION := 8
-export var VELOCITY := Vector3()
-export var TARGET_FPS := 60
-export var GRAVITY :=  4
-
-const MAX_SPEED = 64
+var TYPE = "SHEEP"
+var SPEED = 600
+var VELOCITY = Vector3()
 
 func movement_loop():
 	move_and_slide(Vector3(1, -0.5, 0), Vector3(0,1,0), 0.05, 4, deg2rad(40))
